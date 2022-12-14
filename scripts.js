a9f7ff668f76baef4084c550b86f47ab61589060
rhx1138 <ryanjhendrix@gmail.com>
2022-12-14 11:37:47 -0500

    added increment and decrement, changed styling

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-const counter = document.getElementById('counter');
-const btn = document.getElementById('btn');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+const countEl = document.getElementById('count');
 
+// define the initial count
 let count = 0;
 
-btn.addEventListener('click', () => {
-  count += 1;
-  counter.innerHTML = count;
+// add event listeners to the buttons to update the count
+incrementBtn.addEventListener('click', () => {
+    count++;
+    countEl.innerHTML = count;
+});
+decrementBtn.addEventListener('click', () => {
+    count--;
+    countEl.innerHTML = count;
 });
